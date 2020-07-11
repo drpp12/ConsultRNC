6a685c0773c25e930ac9499ceea6d4a8df588204
drpp12 <deyvi_royer@hotmail.com>
2020-07-11 14:18:42 -0400

    Update coConsultRNC.cs

--- coConsultRNC/coConsultRNC.cs
+++ coConsultRNC/coConsultRNC.cs
@@ -17,63 +17,62 @@ namespace coConsultRNC
         public string Estado { get; set; }
     }
 
-    public class coConsultRNC
+     public string GetNombre(string rncCedula)
     {
-
-        public ResultRnc GetNombre(string rncCedula)
+        string resultado = "";
+        try
         {
-            ResultRnc resultado = new ResultRnc();
-            try
+            var result = new ResultRnc();
+            var client = new RestClient("http://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/");
+            var request = new RestRequest("Rnc.aspx", Method.POST);
+
+            //Esto en realidad es un force. Son valores que ASP.NET espera en el HTTP Request. 
+            //Denle las gracias a Web Forms por esta ;)
+            request.AddParameter("__EVENTTARGET", "");
+            request.AddParameter("__EVENTARGUMENT", "");
+            request.AddParameter("__VIEWSTATE", "/wEPDwUKMTkxNDA2Nzc4Nw9kFgJmD2QWAgIBD2QWAgIDD2QWAmYPZBYCAgEPZBYCAgUPZBYEAgUPPCsADwEADxYEHgtfIURhdGFCb3VuZGceC18hSXRlbUNvdW50ZmRkAgcPPCsADQBkGAIFH2N0bDAwJGNwaE1haW4kZ3ZCdXNjUmF6b25Tb2NpYWwPZ2QFI2N0bDAwJGNwaE1haW4kZHZEYXRvc0NvbnRyaWJ1eWVudGVzD2dkM6A6zdloNYs/efZ4JU/LIN3TDKM=");
+
+            request.AddParameter("__EVENTVALIDATION", "/wEWBQLj6vfLDALqq//bBAKC/r/9AwKhwMi7BAKKnIvVCUYxKuo9/DDpyc38di1xIRjCtI3M");
+
+            //Estos son los valores
+            //  request.AddParameter("rbtnlTipoBusqueda", "0");
+            request.AddParameter("ctl00$cphMain$txtRNCCedula", rncCedula);
+            request.AddParameter("ctl00$cphMain$btnBuscarPorRNC", "Buscar");
+
+            // HTTP Headers
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            var response = client.Execute(request);
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(response.Content);
+            var trs = doc.GetElementbyId("ctl00_cphMain_dvDatosContribuyentes");
+
+            if (trs != null)
             {
-                var result = new ResultRnc();
-                var client = new RestClient("http://www.dgii.gov.do/app/WebApps/Consultas/");
-                var request = new RestRequest("rnc/RncWeb.aspx", Method.POST);
+                var valores = trs.Descendants("td").ToList();
 
-                //Esto en realidad es un force. Son valores que ASP.NET espera en el HTTP Request. 
-                //Denle las gracias a Web Forms por esta ;)
-                request.AddParameter("__EVENTTARGET", "");
-                request.AddParameter("__EVENTARGUMENT", "");
-                request.AddParameter("__LASTFOCUS", "");
-                request.AddParameter("__VIEWSTATE", "/wEPDwUKMTY4ODczNzk2OA9kFgICAQ9kFgQCAQ8QZGQWAWZkAg0PZBYCAgMPPCsACwBkZHTpAYYQQIXs/JET7TFTjBqu3SYU");
-                request.AddParameter("__EVENTVALIDATION", "/wEWBgKl57TuAgKT04WJBAKM04WJBAKDvK/nCAKjwtmSBALGtP74CtBj1Z9nVylTy4C9Okzc2CBMDFcB");
-
-                //Estos son los valores
-                request.AddParameter("rbtnlTipoBusqueda", "0");
-                request.AddParameter("txtRncCed", rncCedula);
-                request.AddParameter("btnBuscaRncCed", "Buscar");
-
-                // HTTP Headers
-                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-
-                var response = client.Execute(request);
-
-                var doc = new HtmlAgilityPack.HtmlDocument();
-                doc.LoadHtml(response.Content);
-                var trs = doc.DocumentNode.Descendants("tr")
-                    .FirstOrDefault(f => f.Attributes.Contains("class") && f.Attributes["class"].Value == "GridItemStyle");
-
-                if (trs != null)
-                {
-                    var valores = trs.Descendants("td").ToList();
-
-                    result.CedulaRnc = valores.First().InnerText;
-                    result.Nombre = valores.Skip(1).First().InnerText;
-                    result.NombreComercial = valores.Skip(2).First().InnerText;
-                    result.Categoria = valores.Skip(3).First().InnerText;
-                    result.RegimenDePago = valores.Skip(4).First().InnerText;
-                    result.Estado = valores.Skip(5).First().InnerText;
-                }
-
-
-                return result;
+                result.CedulaRnc = valores.Skip(1).First().InnerText;
+                result.Nombre = valores.Skip(3).First().InnerText;
+                result.NombreComercial = valores.Skip(5).First().InnerText;
+                result.Categoria = valores.Skip(7).First().InnerText;
+                result.RegimenDePago = valores.Skip(9).First().InnerText;
+                result.Estado = valores.Skip(11).First().InnerText;
             }
-            catch (Exception)
+            resultado = System.Net.WebUtility.HtmlDecode(result.Nombre.ToString());
+            if (result.NombreComercial.ToString() != "&nbsp;")
             {
-
-                return resultado;
+                resultado = System.Net.WebUtility.HtmlDecode(result.NombreComercial.ToString());
             }
+
+            return resultado;
         }
+        catch (Exception)
+        {
 
-
+            return resultado;
+        }
     }
+
+    
 }
